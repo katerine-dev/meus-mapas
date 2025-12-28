ce18e8d04bcdfecf26418d78748afe9ff877e412
Katerine-dev <ka.lwitkoski@gmail.com>
2025-12-28 10:47:06 -0300

    Arruma problemas de renderização

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 export default function Header() {
   return (
     // Header com gradiente roxo da esquerda para direita
-    <header className="from-purple-main via-purple-main to-purple-light bg-linear-to-r px-8 py-1">
+    <header className="bg-gradient-to-r from-purple-main via-purple-main to-purple-light px-8 py-1">
       {/* Container centralizado com largura máxima */}
       <div className="mx-auto flex max-w-6xl items-center justify-between">
         <Image src="/logo.svg" alt="Meus Mapas" width={112} height={112} priority />
