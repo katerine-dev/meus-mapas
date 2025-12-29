b0680f95ebd50973a350a1836fbae65009c9b793
Katerine-dev <ka.lwitkoski@gmail.com>
2025-12-29 16:52:31 -0300

    Corrige o tipo de latitude e longitude

--- app/model/point.ts
+++ app/model/point.ts
@@ -1,10 +1,14 @@
+export interface Location {
+  longitude: number;
+  latitude: number;
+}
+
 export interface Point {
   id: string;
   map_id: string;
   name: string;
   description?: string;
-  latitude: number;
-  longitude: number;
+  location: Location;
   created_at: Date;
   updated_at: Date;
 }
